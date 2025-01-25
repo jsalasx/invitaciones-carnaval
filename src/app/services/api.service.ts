b82f41669cf598a50dbbc3e5694c4648dc4aacf5
Alejandro Salas <joal.sala@gmail.com>
2025-01-25 01:51:02 -0500

    update port

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { Invitado } from '../interfaces/invitado.i';
 })
 export class ApiService {
 
-  private apiUrl = 'https://invitaciones-back.onrender.com/api/invitados'; // URL base de la API de GitHub
+  private apiUrl = 'https://invitaciones-back.onrender.com:3000/api/invitados'; // URL base de la API de GitHub
   constructor(private http: HttpClient) { }
 
   guardarInvitado(invitado: { nombre: string; numAcompanantes: number }): Observable<any> {
